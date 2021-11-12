f928d46151470a81b5823683170dbc4590ec9012
minnitialessio <pasqualealessio.minniti.stud@ispascalcomandini.it>
2021-11-12 08:40:35 +0100

    Inizio Biblioteca

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -8,5 +8,48 @@ namespace Biblioteca
 {
     class Biblioteca
     {
+        private string _nome;
+        private string _indirizzo;
+        private DateTime OrarioApertura { set; get; }
+        private DateTime OrarioChiusura { set; get; }
+
+        public Biblioteca(string nome, string indirizzo, DateTime orarioApertura, DateTime orarioChiusura)
+        {
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            Nome = nome;
+            Indirizzo = indirizzo;
+        }
+        public string Nome
+        {
+            set
+            {
+                if (value == null)
+                    throw new Exception("stringa vuota");
+                _nome = value;
+            }
+            get
+            {
+                return _nome;
+            }
+        }
+        public string Indirizzo
+        {
+            set
+            {
+                if (value == null)
+                    throw new Exception("stringa vuota");
+                _indirizzo = value;
+            }
+            get
+            {
+                return _indirizzo;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Nome + " " + Indirizzo + " " + OrarioApertura + " " + OrarioChiusura;
+        }
     }
 }
