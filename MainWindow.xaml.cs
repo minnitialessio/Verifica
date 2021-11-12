c46d3eb222015cecc880a303252288f0a7a3bed7
minnitialessio <pasqualealessio.minniti.stud@ispascalcomandini.it>
2021-11-12 09:23:04 +0100

    Fine Interfaccia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,26 +32,48 @@ namespace Biblioteca
             libri.Add(libro2);
             libri.Add(libro3);
             biblioteca = new Biblioteca("malatestiana", "via emilia", DateTime.Now, DateTime.Now,libri);
+            lstLibriBiblioteca.ItemsSource = null;
+            lstLibriBiblioteca.ItemsSource = biblioteca.Libri;
         }
 
         private void btnAggiungiLibro_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                string titolo = txtTitoloLibro.Text;
+                string autore = txtAutoreLibro.Text;
+                int numeroPagine = int.Parse(txtNumeroPagine.Text);
+                int anno = int.Parse(txtAnno.Text);
+                string editore = txtEditore.Text;
+                Libro libro = new Libro(autore, titolo, anno, editore, numeroPagine);
+                biblioteca.Libri.Add(libro);
+                lstLibriBiblioteca.ItemsSource = null;
+                lstLibriBiblioteca.ItemsSource = biblioteca.Libri;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnCalcolaNumeroLibri_Click(object sender, RoutedEventArgs e)
         {
-
+            int numeroLibri = biblioteca.CalcolaNumeroLibri();
+            MessageBox.Show(numeroLibri.ToString());
         }
 
         private void btnRicercaLibroDaTitolo_Click(object sender, RoutedEventArgs e)
         {
-
+            string titolo = txtTitolo.Text;
+            Libro libro = biblioteca.RicercaLibroDaTitolo(titolo);
+            txtLibro = libro.ToString();
         }
 
         private void btnRicercaLibriAutore_Click(object sender, RoutedEventArgs e)
         {
-
+            string autore = txtAutore.Text;
+            List<Libro> libriAutore = biblioteca.RicercaLibriAutore(autore);
+            lstLibri.ItemsSource = null;
+            lstLibri.ItemsSource = libriAutore;
         }
 
         private void txtAutore_TextChanged(object sender, TextChangedEventArgs e)
@@ -62,6 +84,19 @@ namespace Biblioteca
         private void txtTitolo_TextChanged(object sender, TextChangedEventArgs e)
         {
             btnRicercaLibroDaTitolo.IsEnabled = true;
+        }
+
+        private void btnReadingTime_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Libro l = lstLibriBiblioteca.SelectedItem as Libro;
+                string readingTime = l.ReadingTime();
+                MessageBox.Show(readingTime);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
