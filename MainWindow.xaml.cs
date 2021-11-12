c1e8666a5fa2ea3ce6cb84d96b30266ad8b2984c
minnitialessio <pasqualealessio.minniti.stud@ispascalcomandini.it>
2021-11-12 09:08:42 +0100

    Inizio Interfaccia

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace Biblioteca
     /// </summary>
     public partial class MainWindow : Window
     {
+        Biblioteca biblioteca;
         public MainWindow()
         {
             InitializeComponent();
+            Libro libro1 = new Libro("Martin","Game of Thrones",2016,"Mondadori",240);
+            Libro libro2 = new Libro("J.K Rowling","Harry Potter",2000, "Mondadori",202);
+            Libro libro3 = new Libro("Autore3","Sherlock Holmes",1998, "Mondadori",300);
+            List<Libro> libri = new List<Libro>();
+            libri.Add(libro1);
+            libri.Add(libro2);
+            libri.Add(libro3);
+            biblioteca = new Biblioteca("malatestiana", "via emilia", DateTime.Now, DateTime.Now,libri);
+        }
+
+        private void btnAggiungiLibro_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCalcolaNumeroLibri_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRicercaLibroDaTitolo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRicercaLibriAutore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtAutore_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnRicercaLibriAutore.IsEnabled = true;
+        }
+
+        private void txtTitolo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            btnRicercaLibroDaTitolo.IsEnabled = true;
         }
     }
 }
