0e95499a1b6c8c702b0bb86b5e20537afca596bc
minnitialessio <pasqualealessio.minniti.stud@ispascalcomandini.it>
2021-11-12 08:49:24 +0100

    Fine Biblioteca

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -12,13 +12,50 @@ namespace Biblioteca
         private string _indirizzo;
         private DateTime OrarioApertura { set; get; }
         private DateTime OrarioChiusura { set; get; }
+        public List<Libro> Libri { set; get; }
 
-        public Biblioteca(string nome, string indirizzo, DateTime orarioApertura, DateTime orarioChiusura)
+        public Biblioteca(string nome, string indirizzo, DateTime orarioApertura, DateTime orarioChiusura, List<Libro> libri)
         {
             OrarioApertura = orarioApertura;
             OrarioChiusura = orarioChiusura;
             Nome = nome;
             Indirizzo = indirizzo;
+            Libri = libri;
+        }
+
+        public void AggiungiLibro(Libro libro)
+        {
+            Libri.Add(libro);
+        }
+
+        public Libro RicercaLibroDaTitolo(string titolo)
+        {
+            foreach(Libro l in Libri)
+            {
+                if(l.Titolo == titolo)
+                {
+                    return l;
+                }
+            }
+            throw new Exception("libro inesistente");
+        }
+
+        public int NumeroLibriPresenti()
+        {
+            return Libri.Count;
+        }
+        public List<Libro> RicercaLibriAutore(string autore)
+        {
+            List<Libro> libriAutore = new List<Libro>();
+
+            foreach(Libro l in Libri)
+            {
+                if(l.Autore == autore)
+                {
+                    libriAutore.Add(l);
+                }
+            }
+            return libriAutore;
         }
         public string Nome
         {
